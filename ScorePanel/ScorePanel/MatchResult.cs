434584888f48057880da7d69948f54724ff77f7e
niniramos@gmail.com <nini217429>
2023-01-17 19:50:35 +0100

    Primera carga de código

---
+++ ScorePanel/ScorePanel/MatchResult.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace ScorePanel
+{
+    public class MatchResult
+    {
+        public string HomeTeam { get; set; }
+        public int HomeTeamResult { get; set; }
+        public string AwayTeam { get; set; }
+        public int AwayTeamResult { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime? EndDate { get; set; }
+
+        public MatchResult()
+        {
+            this.HomeTeamResult = 0;
+            this.AwayTeamResult = 0;
+            this.StartDate = DateTime.Now;
+        }
+
+        public MatchResult(string HomeTeam, string AwayTeam)
+            : this()
+        {
+            this.HomeTeam = HomeTeam;
+            this.AwayTeam = AwayTeam;
+        }
+
+        public void UpdateScore(int homeTeam, int awayTeam)
+        {
+            this.HomeTeamResult = homeTeam;
+            this.AwayTeamResult = awayTeam;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("{0} - {1}: {2}-{3}", this.HomeTeam, this.AwayTeam, this.HomeTeamResult, this.AwayTeamResult);
+        }
+    }
+}
